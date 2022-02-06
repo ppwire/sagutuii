8e16a1edea4df8006a028cba7a170a1f7f3faddf
Poom Muangthai <poom.mth@hotmail.com>
2022-02-06 15:53:53 +0700

    Add timeline module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectsModule } from './projects/projects.module';
+import { TimelinesModule } from './timelines/timelines.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     ProjectsModule,
+    TimelinesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
